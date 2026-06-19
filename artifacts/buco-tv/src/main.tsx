ae95b4ec51fac3d94b339780c355d176c0104be2
ismmaph-rgb <ismmaph@gmail.com>
2026-06-19 12:17:21 -0300

    Update main.tsx

--- artifacts/buco-tv/src/main.tsx
+++ artifacts/buco-tv/src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { setBaseUrl } from "@workspace/api-client-react";
 import App from "./App";
 import "./index.css";
-
-setBaseUrl("https://buco-tv.onrender.com");
 
 createRoot(document.getElementById("root")!).render(<App />);
